58d1bf130d354a2ec99661f659fd615cb32c859d
JeetKaria06 <jeetkaria06@gmail.com>
2021-10-09 12:50:02 +0530

    initializing service

---
+++ client/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/JeetKaria06/orchestrator-service/proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:50001"
+)
+
+func main() {
+	// Setting up a connection to server
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewOrchestratorClient(conn)
+
+	// Contacting server and priting its response
+	name := "Jeet"
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.GetUserByName(ctx, &pb.NameRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not get response because: %v", err)
+	} else {
+		log.Printf("%s", r)
+	}
+}
